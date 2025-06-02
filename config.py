727b66a8cee94b78249d4cc728e661b92e8316fa
charlierasberry <charlie.rasberry@outlook.com>
2025-06-02 17:31:17 +0100

    Started A little widget for windows. And linux.

---
+++ config.py
@@ -0,0 +1,7 @@
+# config.py
+
+HOTKEYS = {
+    '<cmd>+<enter>': r'C:\Program Files\WindowsApps\Microsoft.PowerShell_7.5.1.0_x64__8wekyb3d8bbwe\pwsh.exe',
+    #EXAMPLE '<ctrl>+<alt>+c': r'C:\Windows\System32\calc.exe',
+    #EXAMPLE '<ctrl>+<alt>+p': r'C:\Program Files\Mozilla Firefox\firefox.exe',
+}
